df7bb5745c47f1dac0fc8337d57e95b7bc97f849
Vyacheslav Sayapin <sayapin.exceedteam@gmail.com>
2021-10-22 17:54:59 +0300

    create filter user

--- src/components/Menu/Panel/Loginpanel/LoginPanel.jsx
+++ src/components/Menu/Panel/Loginpanel/LoginPanel.jsx
@@ -28,7 +28,7 @@ const LoginPanel = () => {
                 password
             
             }).then(res => {
-                console.log(res.data)
+
                 if(res.data.token){
                     
 
